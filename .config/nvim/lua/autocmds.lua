3929cd5578b2b22d4f5597f1933558061ae94936
Carlos Álvaro <carlos@cdalvaro.io>
2022-07-09 13:33:32 +0200

    feat(nvim): Run :PackerCompile on file changes

--- .config/nvim/lua/autocmds.lua
+++ .config/nvim/lua/autocmds.lua
@@ -9,6 +9,11 @@ utils.create_augroup({
   { 'BufNewFile,BufRead', '*sls', 'set', 'filetype=sls' }
 }, 'filetype')
 
+-- PackerCompile
+utils.create_augroup({
+  { 'BufWritePost', 'plugins.lua', 'source', '<afile>', '|', 'PackerCompile'}
+}, 'packer_user_config')
+
 -- Restore terminal cursor with vertical line on exit
 autocmd({ 'VimLeave' }, {
   pattern = { '*' },
