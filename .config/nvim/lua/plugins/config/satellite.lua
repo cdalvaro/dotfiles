3a01998b1b512a24b482546dacc61da0ab82e52d
Carlos Álvaro <carlos@cdalvaro.io>
2024-09-21 09:38:18 +0200

    feat(nvim): Update configuration files

--- .config/nvim/lua/plugins/config/satellite.lua
+++
@@ -1,9 +0,0 @@
-local ok, satellite = pcall(require, 'satellite')
-if not ok then
-  return
-end
-
-satellite.setup {
-
-}
-
--- .config/nvim/lua/plugins/init.lua
+++ .config/nvim/lua/plugins/init.lua
@@ -1,9 +1,8 @@
 local packer = require('plugins.packer-init')
 local use = packer.use
 
-return packer.startup {
-  function()
-    use { 'wbthomason/packer.nvim' } -- Let packer manage itself
+return packer.startup {function()
+    use {'wbthomason/packer.nvim'} -- Let packer manage itself
 
     -- Theme
     -- use { -- Catppuccin theme
@@ -11,80 +10,72 @@ return packer.startup {
     --   as = 'catppuccin',
     --   config = 'require "plugins.config.catppuccin"'
     -- }
-    --use { -- Github theme for Neovim and Terminals written in Lua
+    -- use { -- Github theme for Neovim and Terminals written in Lua
     --  'projekt0n/github-nvim-theme',
     --  config = 'require "plugins.config.github-theme"'
-    --}
+    -- }
     use {
-      'Mofiqul/dracula.nvim',
-      config = 'require "plugins.config.dracula"'
+        'Mofiqul/dracula.nvim',
+        config = 'require "plugins.config.dracula"'
     }
 
-    use { 'nvim-lua/popup.nvim' } -- An implementation of the Popup API from vim in Neovim
-    use { 'nvim-lua/plenary.nvim' } -- Useful lua functions used ny lots of plugins
+    use {'nvim-lua/popup.nvim'} -- An implementation of the Popup API from vim in Neovim
+    use {'nvim-lua/plenary.nvim'} -- Useful lua functions used ny lots of plugins
     use { -- Fuzzy finder
-      'nvim-telescope/telescope.nvim',
-      requires = { 'nvim-lua/plenary.nvim' },
-      config = 'require "plugins.config.telescope"'
+        'nvim-telescope/telescope.nvim',
+        requires = {'nvim-lua/plenary.nvim'},
+        config = 'require "plugins.config.telescope"'
     }
     use { -- Syntax highlighting
-      'nvim-treesitter/nvim-treesitter',
-      run = ':TSUpdate',
-      config = 'require "plugins.config.treesitter"'
+        'nvim-treesitter/nvim-treesitter',
+        run = ':TSUpdate',
+        config = 'require "plugins.config.treesitter"'
     }
     use { -- Neovim plugin that allows you to seamlessly manage LSP servers with :LspInstall.
-      'williamboman/nvim-lsp-installer',
-      {
-        'neovim/nvim-lspconfig', -- Quickstart configurations for the Nvim LSP client
+        'williamboman/mason.nvim',
+        requires = {'williamboman/mason-lspconfig.nvim', 'neovim/nvim-lspconfig'},
         config = 'require "plugins.config.lsp-installer"'
-      }
     }
     use {
-      'jose-elias-alvarez/null-ls.nvim',
-      requires = { 'nvim-lua/plenary.nvim' },
+        'jose-elias-alvarez/null-ls.nvim',
+        requires = {'nvim-lua/plenary.nvim'}
     }
-    use { 'xu-cheng/brew.vim' } -- Vim Syntax for Homebrew formulae
-    use { 'kyazdani42/nvim-web-devicons' } -- nvim Devicons
+    use {'xu-cheng/brew.vim'} -- Vim Syntax for Homebrew formulae
+    use {'kyazdani42/nvim-web-devicons'} -- nvim Devicons
     use { -- A blazing fast and easy to configure neovim statusline plugin written in pure lua.
-      'nvim-lualine/lualine.nvim',
-      requires = { 'projekt0n/github-nvim-theme' },
-      config = 'require "plugins.config.lualine"'
+        'nvim-lualine/lualine.nvim',
+        requires = {'projekt0n/github-nvim-theme'},
+        config = 'require "plugins.config.lualine"'
     }
-    use { --faster commenting
-      'numToStr/Comment.nvim',
-      config = 'require "plugins.config.comment"'
+    use { -- faster commenting
+        'numToStr/Comment.nvim',
+        config = 'require "plugins.config.comment"'
     }
     use { -- Git integration for buffers
-      'lewis6991/gitsigns.nvim',
-      requires = { 'nvim-lua/plenary.nvim' },
-      config = 'require "plugins.config.gitsigns"',
+        'lewis6991/gitsigns.nvim',
+        requires = {'nvim-lua/plenary.nvim'},
+        config = 'require "plugins.config.gitsigns"'
     }
-    --use { -- A highly configurable neovim startup screen
+    -- use { -- A highly configurable neovim startup screen
     --  'startup-nvim/startup.nvim',
     --  requires = { "nvim-telescope/telescope.nvim", "nvim-lua/plenary.nvim" },
     --  config = 'require "plugins.config.startup"'
-    --}
+    -- }
 
-    use { 'folke/which-key.nvim' } -- A plugin for Neovim that shows the available keybindings
+    use {'folke/which-key.nvim'} -- A plugin for Neovim that shows the available keybindings
     use { -- Markdown preview for neovim using pandoc and live-server
-      'davidgranstrom/nvim-markdown-preview',
-      config = 'require "plugins.config.markdown-preview"'
+        'davidgranstrom/nvim-markdown-preview',
+        config = 'require "plugins.config.markdown-preview"'
     }
     use { -- Vim plugin for clang-format
-      'rhysd/vim-clang-format',
-      config = 'require "plugins.config.clang-format"'
+        'rhysd/vim-clang-format',
+        config = 'require "plugins.config.clang-format"'
     }
-    -- FIXME: Currently broken
-    --use { -- Neovim plugin that displays decorated scrollbars
-    --  'lewis6991/satellite.nvim',
-    --  config = 'require "plugins.config.satellite"'
-    --}
 
     -- Automatically set up your configuration after cloning packer.nvim
     -- Put this at the end after all plugins
     if _sync_packer then
-      vim.api.nvim_command 'PackerSync'
-      _sync_packer = nil
+        vim.api.nvim_command 'PackerSync'
+        _sync_packer = nil
     end
-  end
-}
+end}
