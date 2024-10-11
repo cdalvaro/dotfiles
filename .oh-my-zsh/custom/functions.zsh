0aabecc196fa839644f749cc831b56c889bf4bff
Carlos Álvaro <carlos.alvaro@meteologica.com>
2024-10-11 13:20:48 +0200

    feat(ssh): Add 's' function for filtering servers with fzf for ssh

--- .oh-my-zsh/custom/functions.zsh
+++ .oh-my-zsh/custom/functions.zsh
@@ -10,3 +10,15 @@ function check_and_source_file() {
   # shellcheck disable=SC1090
   test -f "${file}" && source "${file}"
 }
+
+#---  FUNCTION  -------------------------------------------------------------------------------------------------------
+#          NAME:  s
+#   DESCRIPTION:  Use fzf to filter servers and call ssh with the selected server
+#----------------------------------------------------------------------------------------------------------------------
+function s() {
+  local server
+  server=$(rg '^Host ' ~/.ssh/config | rg -v '\*' | sort | awk '{print $2}' | fzf)
+  if [[-n $server]]; then
+    ssh $server
+  fi
+}
